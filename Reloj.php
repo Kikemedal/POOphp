91df1c2446f34c780defe18343b2e93c5ca374ab
kikemedal <kikemedal@gmail.com>
2024-01-09 18:26:11 +0000

    Se añaden los atributos de la clase

--- Reloj.php
+++ Reloj.php
@@ -1,6 +1,35 @@
 <php 
 
+class Reloj {
+    private $movimiento;
+    private $hermeticidad;
+    private $cristal;
+    private $brazalete;
+    private $corona;
+    private $caja;
+    private $esfera;
+    private $peso;
 
+
+    abstract function getMovimiento();
+    abstract function getHermeticidad();
+    abtract function getCristal();
+    abstract function getBrazalete();
+    abtract function getCorona();
+    abtract function getCaja();
+    abtract function getEsfera();
+    abtract function getPeso();
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
